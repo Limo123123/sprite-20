5013da953e7d74b15950773319c91b5d76891073
Limo123123 <90343619+Limo123123@users.noreply.github.com>
2022-03-12 09:38:39 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,16 @@ input.onButtonPressed(Button.A, function () {
     } else {
         game.removeLife(1)
     }
+    if (sprite2.get(LedSpriteProperty.X) == 2) {
+        game.addScore(1)
+    } else {
+        game.removeLife(1)
+    }
+    if (Sprite.get(LedSpriteProperty.X) == 2) {
+        game.addScore(1)
+    } else {
+        game.removeLife(1)
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     game.resume()
@@ -14,11 +24,24 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     game.pause()
 })
+let sprite2: game.LedSprite = null
 let Sprite: game.LedSprite = null
 Sprite = game.createSprite(2, 2)
-game.setLife(3)
+sprite2 = game.createSprite(2, 0)
+let sprite3 = game.createSprite(2, 4)
+game.setLife(9)
 basic.forever(function () {
     Sprite.move(1)
     basic.pause(500)
     Sprite.ifOnEdgeBounce()
+})
+basic.forever(function () {
+    sprite2.move(2)
+    basic.pause(500)
+    sprite2.ifOnEdgeBounce()
+})
+basic.forever(function () {
+    sprite3.move(2)
+    basic.pause(500)
+    sprite3.ifOnEdgeBounce()
 })
